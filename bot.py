32303d9483be1d1f242b34cadaab906ec05c85ee
Wario <brettjayp@gmail.com>
2026-04-26 10:28:34 -0600

    partial refactor, fix subrpocess calls

--- bot.py
+++ bot.py
@@ -4,11 +4,13 @@ import os
 
 from dotenv import load_dotenv
 
+
 # get secrets
 load_dotenv()
 botToken = os.getenv('botToken')
-broadcast = os.getenv('broadcastChannel')
-windrose = os.getenv('windroseServerExe')
+broadcast = int(os.getenv('broadcastChannel'))
+windrose = rf'{os.getenv('windroseServerExe')}'
+
 
 # discord.py setup stuff
 intents = discord.Intents.default()
@@ -17,6 +19,27 @@ intents.members = True
 
 client = discord.Client(intents=intents)
 
+
+# subprocess helper
+# TODO: switch case, check system for existing process, open process outside of python task tree
+def pWindrose(command):
+	global windrosePID
+	if command == 'up':
+		global procWindrose
+		procWindrose = subprocess.Popen([windrose], creationflags=subprocess.DETACHED_PROCESS)
+		windrosePID = int(procWindrose.pid)
+		return windrosePID
+
+	if command == 'down':
+		ifprocWindrose:
+			procWindrose.terminate()
+			windrosePID = 0
+		return windrosePID
+
+	if command == 'status':
+		return windrosePID
+
+
 # listener
 @client.event
 async def on_ready():
@@ -24,26 +47,42 @@ async def on_ready():
 
 	channel = client.get_channel(broadcast)
 	if channel:
-		await channel.send('`wariobot` is online.\n\nPrefix all commands with `!`.\nCurrent commands are: `hello`, `windrose up`, `windrose down`')
+		await channel.send('`wariobot` is online.\nPrefix all commands with `!`. wr = windrose\nCurrent commands are: `!hello`, `!wr status`, `!wr up`, `!wr down`')
 	else:
 		print('Channel not found.')
 
 
 @client.event
 async def on_message(message):
+	status = pWindrose('status')
+
 	if message.author == client.user:
 		return
 
 	if message.content.startswith('!hello'):
 		await message.channel.send(f'Yo what up, {message.author}?')
 
-	if message.content.startswith('!windrose up'):
-		pWindrose = subprocess.Popen([windrose])
-		await message.channel.send('Starting Windrose Dedotated Server.')
+	if message.content.startswith('!wr status'):
+		if status == 0:
+			await message.channel.send('A Windrose dedotated server is not currently detected.')
+		else:
+			await message.channel.send(f'A Windrose dedotated server is detected with PID: {status}')
 
-	if message.content.startswith('!windrose down'):
-		pWindrose.terminate()
-		await.message.channel.send('Ending Windrose Dedotated Server.')
+	if message.content.startswith('!wr up'):
+		if status = 0:
+			pWindrose('up')
+			status = pWindrose('status')
+			await message.channel.send(f'Starting the Windrose dedotated server. Process started with PID: {status}')
+		else:
+			await message.channel.send(f'A Windrose dedotated server is already running with PID: {status}')
+
+	if message.content.startswith('!wr down'):
+		if status != 0:
+			pWindrose('down')
+			await message.channel.send(f'Shutting down the Windrose dedotated server at PID: {status}')
+			status = pWindrose('status')
+		else:
+			await message.channel.send('No Windrose dedotated server was detected.')
+
 
 client.run(botToken)
-
